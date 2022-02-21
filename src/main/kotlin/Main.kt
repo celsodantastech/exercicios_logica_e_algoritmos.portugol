02d96b292e1baec813ad9a2127a1c0194d928079
celsodantasdev <celsodantasdev@gmail.com>
2022-02-21 13:17:16 -0300

    novo exercício

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    println("Geraldo - Oba!")
+    println("Rodolfo - Opa!")
+    println("Geraldo - Bão?")
+    println("Rodolfo - Bão, e ocê, sô?")
+    println("Geraldo - Bão também.. :( ")
+    println("Rodolfo - E cadê a cumadi?")
+    println("Geraldo - Homi, cê num sabe..")
+    println("Rodolfo - Sei mesmo não..")
+    println("Geraldo - Acredita que roubaram meu carro?")
+    println("Rodolfo - Mentira?? :)")
+    println("Geraldo - Pois é, homem, eu tô arrasado")
+    println("Rodolfo - Onde foi isso??")
+    println("Geraldo - Foi lá perto da padaria do Ântonio")
+    println("Rodolfo - Nossa, eu conheço bastante gente naquela redondeza, vou perguntar pros meus conhecidos")
+    println("Geraldo - Nossa! Obrigado! Qualquer coisa me avise.")
+}
